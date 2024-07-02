55fdf96f6c9e3d9461d319452fb98a92f4b858e2
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-07-03 06:07:57 +0800

    Update diy02.sh

--- diy/diy02.sh
+++ diy/diy02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #1. 默认ip
-#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.12/g' openwrt/package/base-files/files/bin/config_generate
 
 # 修改密码
 sed -i 's/root:::0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
